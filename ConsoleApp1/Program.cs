8fc3fa86f690e9875119844eda7735453bae6829
user <user@STAA>
2023-10-05 16:27:59 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    public enum VorchlivostLevel
+    {
+        Низкий,
+        Средний,
+        Высокий
+    }
+    public struct Ded
+    {
+        public string Имя;
+        public VorchlivostLevel УровеньВорчливости;
+        public string[] ФразыДляВорчания;
+        public int КоличествоСиняков;
+        public Ded(string имя, VorchlivostLevel уровеньВорчливости, string[] фразыДляВорчания)
+        {
+            Имя = имя;
+            УровеньВорчливости = уровеньВорчливости;
+            ФразыДляВорчания = фразыДляВорчания;
+            КоличествоСиняков = 0;
+        }
+
+        public int ПроверкаМатерныхСлов(params string[] матерныеСлова)
+        {
+            int количествоФингалов = 0;
+
+            foreach (string фраза in ФразыДляВорчания)
+            {
+                foreach (string матерноеСлово in матерныеСлова)
+                {
+                    if (фраза.Contains(матерноеСлово))
+                    {
+                        КоличествоСиняков++;
+                        количествоФингалов++;
+                    }
+                }
+            }
+
+            return количествоФингалов;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Ded[] деды = new Ded[]
+            {
+            new Ded("Дед1", VorchlivostLevel.Высокий, new string[] {"Проститутки!", "Гады!", "Бяка!"}),
+            new Ded("Дед2", VorchlivostLevel.Низкий, new string[] {"Погода отвратительная.", "Нет сил вставать."}),
+            new Ded("Дед3", VorchlivostLevel.Средний, new string[] {"Старая техника!", "Молодежь тупая."}),
+            new Ded("Дед4", VorchlivostLevel.Высокий, new string[] {"Электроника!"}),
+            new Ded("Дед5", VorchlivostLevel.Средний, new string[] {"Природа заменяется бетоном."})
+            };
+
+            Console.WriteLine("Введите матерные слова через пробел: ");
+            string[] матерныеСлова = Console.ReadLine().Split(' ');
+
+            int общееКоличествоФингалов = 0;
+
+            foreach (Ded дед in деды)
+            {
+                int количествоФингалов = дед.ПроверкаМатерныхСлов(матерныеСлова);
+                общееКоличествоФингалов += количествоФингалов;
+
+                Console.WriteLine($"{дед.Имя}: {количествоФингалов} фингалов");
+            }
+
+            Console.WriteLine($"Общее количество фингалов: {общееКоличествоФингалов}");
+        }
+    }
+
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            {
+                new Ded("Дед1", VorchlivostLevel.Высокий, new string[] { "Проститутки!", "Гады!", "Бяка!" }),
+            new Ded("Дед2", VorchlivostLevel.Низкий, new string[] { "Погода отвратительная.", "Нет сил вставать." }),
+            new Ded("Дед3", VorchlivostLevel.Средний, new string[] { "Старая техника!", "Молодежь тупая." }),
+            new Ded("Дед4", VorchlivostLevel.Высокий, new string[] { "Электроника!" }),
+            new Ded("Дед5", VorchlivostLevel.Средний, new string[] { "Природа заменяется бетоном." })
+        };
+
+            Console.WriteLine("Введите матерные слова через пробел: ");
+            string[] матерныеСлова = Console.ReadLine().Split(' ');
+
+            int общееКоличествоФингалов = 0;
+
+            foreach (Ded дед in дед)
+            {
+                int количествоФингалов = дед.ПроверкаМатерныхСлов(матерныеСлова);
+                общееКоличествоФингалов += количествоФингалов;
+
+                Console.WriteLine($"{дед.Имя}: {количествоФингалов} фингалов");
+            }
+
+            Console.WriteLine($"Общее количество фингалов: {общееКоличествоФингалов}");
+        }
+
+    }
+    
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,124 @@
+﻿
+
+using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    { static int FindMax(int a, int b)
+        {
+            return (a > b) ? a : b;
+        }
+    
+        static void SwapNumbers(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        static bool CalculateFactorial(int n, out long res)
+        {
+            try
+            {
+                checked
+                {
+                    res = 1;
+                    for (int i = 2; i <= n; i++)
+                    {
+                        res *= i;
+                    }
+                }
+                return true;
+            }
+            catch (OverflowException)
+            {
+                res = 0;
+                return false;
+            }
+        static bool CalculateFact(int n, out long res)
+            static long (int n)
+            {
+                if (n == 0 || n == 1) return 1;   
+                return n * (n - 1); // рекрусивный случай
+            }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("лабораторная тумакова 5.1");
+                Console.WriteLine("Введите первое число: ");
+                if (int.TryParse(Console.ReadLine(), out int n1))
+                {
+                    Console.WriteLine("введите второе число: ");
+                    if (int.TryParse(Console.ReadLine(), out int n2))
+                    {
+                        int result = FindMax(n1, n2);
+                        Console.WriteLine("Наибольшее число: " + result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка ввода второго числа.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода первого числа.");
+                }
+
+                Console.WriteLine("лабораторная тумакова 5.2");
+                Console.WriteLine("Введите первое число: ");
+                if (int.TryParse(Console.ReadLine(), out int num1))
+                {
+                    Console.WriteLine("Введите второе число: ");
+                    if (int.TryParse(Console.ReadLine(), out int num2))
+                    {
+                        SwapNumbers(ref num1, ref num2);
+                        Console.WriteLine("После обмена:");
+                        Console.WriteLine("Первое число: " + num1);
+                        Console.WriteLine("Второе число: " + num2);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка ввода второго числа.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода первого числа.");
+                }
+
+                Console.WriteLine("лабораторная тумакова 5.3");
+                Console.WriteLine("Введите число для вычисления факториала: ");
+                if (int.TryParse(Console.ReadLine(), out int inputNumber))
+                {
+                    if (CalculateFactorial(inputNumber, out long result))
+                    {
+                        Console.WriteLine($"Факториал числа {inputNumber} равен: {result}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка: переполнение в процессе вычисления факториала.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода числа.");
+                }
+
+                Console.WriteLine("лабораторная тумакова 5.4");
+                Console.WriteLine("Введите число для вычисления факториала: ");
+                if (int.TryParse(Console.ReadLine(), out int inputNumb))
+                {
+                    long result = CalculateFact(inputNumber);
+                    Console.WriteLine($"Факториал числа {inputNumber} равен: {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода числа.");
+                }
+                // чет в этот полно ошибок, я растерялась, не пойму, что исправлять....
+            }
+
+        }
+        
+    }
+}
+
